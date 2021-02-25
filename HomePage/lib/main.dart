efc1aea86af1f1debb611f7ba97136f61479a1f2
Pathum312 <pathum.2019557@iit.ac.lk>
2021-02-26 00:59:07 +0530

    Started Actual Implementation of home page

--- HomePage/lib/main.dart
+++ HomePage/lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(myApp());
+void main() => runApp(homePage());
 
-class myApp extends StatelessWidget {
+class homePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Design Tut',
+      title: 'Home Page',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Roboto'),
+      theme: ThemeData(fontFamily: 'Actor'),
       home: MyHomePage(),
     );
   }
@@ -19,133 +19,136 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                new Container(
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Color(0xFF18D191)),
-                  child: new Icon(Icons.local_offer, color: Colors.white),
-                ),
-                new Container(
-                  margin: new EdgeInsets.only(right: 50.0, top: 50.0),
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Color(0xFFFC6A7F)),
-                  child: new Icon(Icons.home, color: Colors.white),
-                ),
-                new Container(
-                  margin: new EdgeInsets.only(left: 30.0, top: 50.0),
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Color(0xFFFFCE56)),
-                  child: new Icon(Icons.local_car_wash, color: Colors.white),
-                ),
-                new Container(
-                  margin: new EdgeInsets.only(left: 90.0, top: 40.0),
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Color(0xFF45E0EC)),
-                  child: new Icon(Icons.place, color: Colors.white),
-                )
-              ],
-            ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0, bottom: 200.0),
-                  child: new Text(
-                    'Flutter Designs',
-                    style: new TextStyle(fontSize: 30.0),
+        child: Container(
+          color: Color(0xFFF5F5FA),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  new Container(
+                    height: 60.0,
+                    width: 60.0,
+                    decoration: new BoxDecoration(
+                        borderRadius: new BorderRadius.circular(50.0),
+                        color: Color(0xFF18D191)),
+                    child: new Icon(Icons.local_offer, color: Colors.white),
                   ),
-                )
-              ],
-            ),
-            new Row(
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 20.0, right: 10.0, top: 10.0),
-                    child: new Container(
-                      alignment: Alignment.center,
-                      height: 60.0,
-                      decoration: new BoxDecoration(
-                        color: Color(0xFF18D191),
-                        borderRadius: new BorderRadius.circular(10.0),
+                  new Container(
+                    margin: new EdgeInsets.only(right: 50.0, top: 50.0),
+                    height: 60.0,
+                    width: 60.0,
+                    decoration: new BoxDecoration(
+                        borderRadius: new BorderRadius.circular(50.0),
+                        color: Color(0xFFFC6A7F)),
+                    child: new Icon(Icons.home, color: Colors.white),
+                  ),
+                  new Container(
+                    margin: new EdgeInsets.only(left: 30.0, top: 50.0),
+                    height: 60.0,
+                    width: 60.0,
+                    decoration: new BoxDecoration(
+                        borderRadius: new BorderRadius.circular(50.0),
+                        color: Color(0xFFFFCE56)),
+                    child: new Icon(Icons.local_car_wash, color: Colors.white),
+                  ),
+                  new Container(
+                    margin: new EdgeInsets.only(left: 90.0, top: 40.0),
+                    height: 60.0,
+                    width: 60.0,
+                    decoration: new BoxDecoration(
+                        borderRadius: new BorderRadius.circular(50.0),
+                        color: Color(0xFF45E0EC)),
+                    child: new Icon(Icons.place, color: Colors.white),
+                  )
+                ],
+              ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0, bottom: 200.0),
+                    child: new Text(
+                      'Flutter Designs',
+                      style: new TextStyle(fontSize: 30.0),
+                    ),
+                  )
+                ],
+              ),
+              new Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 20.0, right: 10.0, top: 10.0),
+                      child: new Container(
+                        alignment: Alignment.center,
+                        height: 60.0,
+                        decoration: new BoxDecoration(
+                          color: Color(0xFF18D191),
+                          borderRadius: new BorderRadius.circular(10.0),
+                        ),
+                        child: new Text(
+                          'Sign In With Email',
+                          style: new TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
-                      child: new Text(
-                        'Sign In With Email',
-                        style: new TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+              new Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 20.0, right: 5.0, top: 10.0),
+                      child: new Container(
+                        alignment: Alignment.center,
+                        height: 60.0,
+                        decoration: new BoxDecoration(
+                          color: Color(0xFF4364A1),
+                          borderRadius: new BorderRadius.circular(10.0),
+                        ),
+                        child: new Text(
+                          'FaceBook',
+                          style: new TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                )
-              ],
-            ),
-            new Row(
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 20.0, right: 5.0, top: 10.0),
-                    child: new Container(
-                      alignment: Alignment.center,
-                      height: 60.0,
-                      decoration: new BoxDecoration(
-                        color: Color(0xFF4364A1),
-                        borderRadius: new BorderRadius.circular(10.0),
-                      ),
-                      child: new Text(
-                        'FaceBook',
-                        style: new TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.white,
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          left: 10.0, right: 10.0, top: 10.0),
+                      child: new Container(
+                        alignment: Alignment.center,
+                        height: 60.0,
+                        decoration: new BoxDecoration(
+                          color: Color(0xFFDF513B),
+                          borderRadius: new BorderRadius.circular(10.0),
+                        ),
+                        child: new Text(
+                          'Google',
+                          style: new TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 10.0, right: 10.0, top: 10.0),
-                    child: new Container(
-                      alignment: Alignment.center,
-                      height: 60.0,
-                      decoration: new BoxDecoration(
-                        color: Color(0xFFDF513B),
-                        borderRadius: new BorderRadius.circular(10.0),
-                      ),
-                      child: new Text(
-                        'Google',
-                        style: new TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            )
-          ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
