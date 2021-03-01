ab18e198febf0a295139da60dea7558f4481fb18
Pathum312 <pathum.2019557@iit.ac.lk>
2021-03-02 02:08:49 +0530

    added python file and dataset

---
+++ breedModel_creation.py
@@ -0,0 +1,12 @@
+# Imports
+
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import cv2 as cv
+import os
+import numpy as np
+
+# Loading the data into arrays
+
+
