e14a0dc84fa9bcf8f52974def3fd42032656b1d2
Pathum312 <pathum.2019557@iit.ac.lk>
2021-03-07 17:50:39 +0530

    Breed_Model.h5 has 70% acc now

--- BreedModel_CNN.py
+++ BreedModel_CNN.py
@@ -52,17 +52,17 @@ val_generator = test_datagen.flow_from_directory(
 # CNN Model
 
 model = Sequential()
-model.add(Conv2D(1024, (3, 3), input_shape=input_shape))
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.summary()
 
-model.add(Conv2D(1024, (3, 3)))
+model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(2048, (3, 3)))
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
