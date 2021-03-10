f7ffdc8bc6ec2d2da65c66d3c04375d975226bb9
Pathum312 <pathum.2019557@iit.ac.lk>
2021-03-10 23:16:46 +0530

    Added the add() method and a route

--- Tobi/Backend/AccountDatabase.py
+++ Tobi/Backend/AccountDatabase.py
@@ -1,24 +1,35 @@
 # Imports
+
 from flask import Flask, jsonify, request
+from firebase import firebase
+
+# Connection to the User-Credentials database
+
+dbConeection = firebase.FirebaseApplication('https://test-3f1bf-default-rtdb.firebaseio.com/', None)
 
 # The user credentials database will be accessed here, and the other classes will import the required data
+
 class AccountDatabase:
 
-    # User Credentials
-
-    __username = ""
-    __password = ""
-    __email = ""
-
     # Add user credentials function
-    def add(self):
-        return self
+
+    def add(fName, lName, username, password, email):
+        data = {
+            'fName':fName,
+            'lName':lName,
+            'Email':email,
+            'Username':username,
+            'Password':password
+        }
+        dbConeection.post('test-3f1bf-default-rtdb/User-Credentials', data)
 
     # Delete user credentials function
+
     def delete(self):
         return self
 
     # Update user credentials function
+
     def update(self):
         return self
 
--- Tobi/Backend/SignUp.py
+++ Tobi/Backend/SignUp.py
@@ -1,15 +1,40 @@
 # Imports
-from flask import Flask, jsonify, request
+
+from AccountDatabase import AccountDatabase
+from flask import Flask, jsonify
+
+# Flask app
+
+app = Flask(__name__)
+
+# User Credentials
+
+__fName = "Pathum"
+__lName = "Senanayake"
+__username = "Pathum312"
+__password = "#Pinky@2299"
+__email = "pathum@hehe"
 
 class SignUp:
 
-    # User Credentials
-
-    __username = ""
-    __password = ""
-    __email = ""
-
     # User account creation function
-    def createAccount(self):
-        return self
+    def createAccount(__fName, __lName, __username, __password, __email):
+        AccountDatabase.add(fName=__fName, lName=__lName, username=__username, password=__password, email=__email)
+        data = {
+            'Account':"Created"
+        }
+        return data
+
+# Route
+@app.route('/sign-up', methods=['GET'])
+def signUp():
+    result = SignUp.createAccount(__fName, __lName, __username, __password, __email)
+    return jsonify(result)
+
+# Running the flask app
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
+
 
