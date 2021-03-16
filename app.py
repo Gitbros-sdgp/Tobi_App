f04dd8466deb00c141105fefeafc72c553b9026d
Pathum312 <pathum.2019557@iit.ac.lk>
2021-03-17 00:26:37 +0530

    change

--- app.py
+++ app.py
@@ -44,14 +44,16 @@ def login():
     # login_username = ''
     # login_password = ''
 
+    global login_username, login_password
+
     if request.method == 'POST':
         login_username = request.form.get('__uname')
         login_password = request.form.get('__password')
 
-    if request.method == 'GET':
-        result = log.verifyAccount(login_username, login_password)
+        if request.method == 'GET':
+            result = log.verifyAccount(login_username, login_password)
 
-        return jsonify(result)
+            return jsonify(result)
     else:
         data = {
             'Error': 'Error 405 - Method Not Allowed'
