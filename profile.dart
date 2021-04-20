3ab85a395339d4ce6ec16add3d56faf53e5bcf23
sumudusujith <sumudu.2019582@iit.ac.lk>
2021-04-20 19:00:04 +0530

    p2

--- profile.dart
+++ profile.dart
@@ -23,3 +23,73 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.init(context, height: 896, width: 414, allowFontScaling: true);
+
+    var profileInfo = Expanded(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: kSpacingUnit.w * 10,
+            width: kSpacingUnit.w * 10,
+            margin: EdgeInsets.only(top: kSpacingUnit.w * 3),
+            child: Stack(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: kSpacingUnit.w * 5,
+                  backgroundImage: AssetImage('assets/images/jjj12.jpg'),
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Container(
+                    height: kSpacingUnit.w * 2.5,
+                    width: kSpacingUnit.w * 2.5,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).accentColor,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      heightFactor: kSpacingUnit.w * 1.5,
+                      widthFactor: kSpacingUnit.w * 1.5,
+                      child: Icon(
+                        LineAwesomeIcons.pen,
+                        color: kDarkPrimaryColor,
+                        size: ScreenUtil().setSp(kSpacingUnit.w * 1.5),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: kSpacingUnit.w * 2),
+          Text(
+            'Sumudu Herath',
+            style: kTitleTextStyle,
+          ),
+          SizedBox(height: kSpacingUnit.w * 0.5),
+          Text(
+            'sumudusujith@gmail.com',
+            style: kCaptionTextStyle,
+          ),
+          SizedBox(height: kSpacingUnit.w * 2),
+          Container(
+            height: kSpacingUnit.w * 4,
+            width: kSpacingUnit.w * 20,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(kSpacingUnit.w * 3),
+              color: Theme.of(context).accentColor,
+            ),
+            child: Center(
+              child: Text(
+                'My Profile',
+                style: kButtonTextStyle,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
