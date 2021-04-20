26ac53156a0b45730a23067a6b03d47cd8c69a39
sumudusujith <sumudu.2019582@iit.ac.lk>
2021-04-20 22:07:33 +0530

    c9

--- chat page _new/ChatScreen.dart
+++ chat page _new/ChatScreen.dart
@@ -1,3 +1,17 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/ChatMessageListItem.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:image_picker/image_picker.dart';
+
 
 final googleSignIn = new GoogleSignIn();
 final analytics = new FirebaseAnalytics();
@@ -156,3 +170,42 @@ class ChatScreenState extends State<ChatScreen> {
     await _ensureLoggedIn();
     _sendMessage(messageText: text, imageUrl: null);
   }
+
+  void _sendMessage({String messageText, String imageUrl}) {
+    reference.push().set({
+      'text': messageText,
+      'email': googleSignIn.currentUser.email,
+      'imageUrl': imageUrl,
+      'senderName': googleSignIn.currentUser.displayName,
+      'senderPhotoUrl': googleSignIn.currentUser.photoUrl,
+    });
+
+    analytics.logEvent(name: 'send_message');
+  }
+
+  Future<Null> _ensureLoggedIn() async {
+    GoogleSignInAccount signedInUser = googleSignIn.currentUser;
+    if (signedInUser == null)
+      signedInUser = await googleSignIn.signInSilently();
+    if (signedInUser == null) {
+      await googleSignIn.signIn();
+      analytics.logLogin();
+    }
+
+    currentUserEmail = googleSignIn.currentUser.email;
+
+    if (await auth.currentUser() == null) {
+      GoogleSignInAuthentication credentials =
+          await googleSignIn.currentUser.authentication;
+      await auth.signInWithGoogle(
+          idToken: credentials.idToken, accessToken: credentials.accessToken);
+    }
+  }
+
+  Future _signOut() async {
+    await auth.signOut();
+    googleSignIn.signOut();
+    Scaffold.of(_scaffoldContext)
+        .showSnackBar(new SnackBar(content: new Text('User logged out')));
+  }
+}
