def91928fc5a2c9d0fe891f94233880797b7741f
sumudusujith <sumudu.2019582@iit.ac.lk>
2021-04-20 18:58:03 +0530

    p1

---
+++ profile.dart
@@ -0,0 +1,25 @@
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ThemeProvider(
+      initTheme: kDarkTheme,
+      child: Builder(
+        builder: (context) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeProvider.of(context),
+            home: ProfileScreen(),
+          );
+        },
+      ),
+    );
+  }
+}
