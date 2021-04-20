f4d65fec3cbacbf7816006f6ceeaf11bd3277ce4
sumudusujith <sumudu.2019582@iit.ac.lk>
2021-04-20 22:04:09 +0530

    c4

---
+++ chat page _new/ChatScreen.dart
@@ -0,0 +1,19 @@
+
+final googleSignIn = new GoogleSignIn();
+final analytics = new FirebaseAnalytics();
+final auth = FirebaseAuth.instance;
+var currentUserEmail;
+var _scaffoldContext;
+
+class ChatScreen extends StatefulWidget {
+  @override
+  ChatScreenState createState() {
+    return new ChatScreenState();
+  }
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textEditingController =
+      new TextEditingController();
+  bool _isComposingMessage = false;
+  final reference = FirebaseDatabase.instance.reference().child('messages');
